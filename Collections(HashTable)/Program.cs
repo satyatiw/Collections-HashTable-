17448bac550be2487f9649a32253f55bda24be69
satyatiw <40672391+satyatiw@users.noreply.github.com>
2019-09-23 17:21:29 +0530

    Update Program.cs

--- Collections(HashTable)/Program.cs
+++ Collections(HashTable)/Program.cs
@@ -10,6 +10,59 @@ namespace Collections_HashTable_
     {
         static void Main(string[] args)
         {
+            
+            Hashtable ht = new Hashtable()
+                {
+                    { 1, "One" },
+                    { 2, "Two" },
+                    { 3, "Three" },
+                    { 4, "Four" },
+                    { 5, null },
+                    { "Fv", "Five" },
+                    { 8.5F, 8.5 }
+                };
+
+
+            //for itirating the full list and dispaying all key and value.
+            //method 1
+             foreach (DictionaryEntry item in ht)
+                 Console.WriteLine("key:{0}, value:{1}", item.Key, item.Value);
+             Console.ReadLine();
+
+
+            //method 2
+            /*  foreach (var key in ht.Keys)
+                  Console.WriteLine("Key:{0}, Value:{1}", key, ht[key]);
+              Console.ReadLine(); */
+
+
+            //method 3(for displaying only the values)
+            /* foreach (var value in ht.Values)
+                 Console.WriteLine("Value:{0}", value);
+             Console.ReadLine();  */
+
+
+            //Remove the particular element.
+            ht.Remove("Fv");
+            foreach (DictionaryEntry item in ht)
+                Console.WriteLine("key:{0}, value:{1}", item.Key, item.Value);
+            Console.ReadLine();
+
+
+            //Check if HashTable contains that particular Element.
+            ht.Contains(2);
+            foreach (DictionaryEntry item in ht)
+                Console.WriteLine("key:{0}, value:{1}", item.Key, item.Value);
+            Console.ReadLine();
+
+            
+            
+            // removes all elements
+            ht.Clear(); 
+            Console.WriteLine("Total Elements: {0}", ht.Count);
+            foreach (DictionaryEntry item in ht)
+                Console.WriteLine("key:{0}, value:{1}", item.Key, item.Value);
+            Console.ReadLine();
         }
     }
 }
